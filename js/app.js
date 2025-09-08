1a5759c27ccf3807acf0ebc8382749bde053d12b
Gonzalo Consorti <gonzalo.consorti@bue.edu.ar>
2025-09-08 15:48:09 -0300

    commit

---
+++ js/app.js
@@ -0,0 +1,19 @@
+// Este archivo te permite agregar los platos de la carta de forma dinámica.
+// Puedes integrarlo en tu index.html si deseas que la carta se genere desde JavaScript.
+
+const carta = {
+    entradas: [
+        { nombre: "Ensalada mixta", precio: 5 },
+        { nombre: "Empanadas", precio: 4 }
+    ],
+    principales: [
+        { nombre: "Milanesa con papas", precio: 12 },
+        { nombre: "Pollo al horno", precio: 11 }
+    ],
+    postres: [
+        { nombre: "Flan casero", precio: 5 },
+        { nombre: "Helado", precio: 4 }
+    ]
+};
+
+// Si quieres mostrar la carta dinámicamente, puedes agregar un script dentro de index.html
